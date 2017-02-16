1672ba84909272ebcbd06b9ff3b9d4b55378e6e9
Jason Hinebaugh <jason.hinebaugh@customink.com>
2017-02-16 08:55:15 -0500

    Add usage prompt to help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,8 +3,23 @@ const proc = require('../proc');
 const initialize = require('../init');
 const {genericErrorHandler} = require('../utils');
 
+const boilerplate = `
+USAGE
+asphalt command [arguments...]
+
+COMMANDS
+create type     Create a new record of the given type. This will prompt for each field of the new record.
+status type     Show the status of all records of the given type.
+show type id    Show details about the record of the given type with the given id.
+help            Display this message
+`;
+
+function createTypeList(store) {
+  return Object.keys(store).reduce((acc, key) => `${acc}${key}\n`, '\nTYPES\n');
+}
+
 module.exports = function help(schema, args) {
   return initialize().then(init => {
-    proc.stdout.write('TODO: Add usage example\n');
+    proc.stdout.write(boilerplate + createTypeList(init.store));
   }).catch(genericErrorHandler);
 };
